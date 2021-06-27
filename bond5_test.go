176a1a4292a212d139be884adc7a69d4033c752e
Luiz <luizhamilton29@gmail.com>
2021-06-27 09:47:26 -0300

    Added more test cases and updated code to be able to add more tests

--- bond5_test.go
+++ bond5_test.go
@@ -1,8 +1,6 @@
 package financial
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"math/big"
 	"math/rand"
@@ -16,266 +14,212 @@ import (
 	"github.com/consensys/gnark/backend"
 	"github.com/consensys/gnark/backend/groth16"
 	"github.com/consensys/gnark/frontend"
-
-	"github.com/shopspring/decimal"
 )
-
-type Bond struct {
-	Isin   string
-	Size   string
-	Ticker string
-}
-
-func setQuotesValue(bond Bond, quoteA string, quoteB string, quoteC string) [3][]byte {
-	quote0, err := decimal.NewFromString(quoteA)
-	quote1, err := decimal.NewFromString(quoteB)
-	quote2, err := decimal.NewFromString(quoteC)
-	if err != nil {
-		panic(err)
-	}
-
-	one100 := decimal.NewFromInt(100)
-	bondSize, err := decimal.NewFromString(bond.Size)
-
-	toRet := [3][]byte{}
-	// 93.63 / 100 = 0,9363
-	quote0 = quote0.Div(one100)
-	quote1 = quote1.Div(one100)
-	quote2 = quote2.Div(one100)
-
-	//0.9363 * 550000 = 514965
-	quote0 = bondSize.Mul(quote0)
-	quote1 = bondSize.Mul(quote1)
-	quote2 = bondSize.Mul(quote2)
-
-	//convert to cents * 100
-	// 514965 * 100 = 51496500
-	quote0 = quote0.Mul(one100)
-	quote1 = quote1.Mul(one100)
-	quote2 = quote2.Mul(one100)
-
-	toRet[0] = quote0.BigInt().Bytes()
-	toRet[1] = quote1.BigInt().Bytes()
-	toRet[2] = quote2.BigInt().Bytes()
-
-	return toRet
-}
 
 func TestBondv5(t *testing.T) {
 
-	//Isin hash
-	var bond Bond
-	bond.Isin = "CA29250NAT24"
-	bond.Size = "550000"
-	bond.Ticker = "ENB 5.375 27-Sep-2077"
-
-	reqBodyBytes := new(bytes.Buffer)
-	json.NewEncoder(reqBodyBytes).Encode(bond)
-
-	hashFunc := hash.MIMC_BN254
-
-	goMimc := hashFunc.New("seed")
-	goMimc.Write([]byte(reqBodyBytes.Bytes()))
-	var IsinHash = goMimc.Sum(nil)
-
-	//fmt.Print(IsinHash)
-
+	/**
+	*  First step: Compile and Setup circuit.
+	 */
 	var circuit bondCircuitv5
-
 	// compiles our circuit into a R1CS
+	fmt.Println("Compiling Bond circuit")
 	r1cs, err := frontend.Compile(ecc.BN254, backend.GROTH16, &circuit)
+	fmt.Println("Setting up circuit - it will take some time")
 	pk, vk, err := groth16.Setup(r1cs)
-
+	fmt.Println("pk and vk created. Now starting testing")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	//setup signature parameters
-	signature.Register(signature.EDDSA_BN254, eddsabn254.GenerateKeyInterfaces)
+	/*
+	* Populate test cases
+	 */
+	var testCases = createTestCases()
 
-	// Create a private/pub key to sign
-	hFunc := hash.MIMC_BN254.New("seed")
-	src1 := rand.NewSource(1)
-	src2 := rand.NewSource(2)
-	src3 := rand.NewSource(3)
-	rA := rand.New(src1)
-	rB := rand.New(src2)
-	rC := rand.New(src3)
+	for i := 0; i < 3; i++ {
+		fmt.Println("Test ", i)
+		testCase := testCases[i]
+		/*
+		* Hash and Signatures
+		 */
+		hashFunc := hash.MIMC_BN254
+		goMimc := hashFunc.New("seed")
+		signature.Register(signature.EDDSA_BN254, eddsabn254.GenerateKeyInterfaces)
 
-	privKeyA, err := signature.EDDSA_BN254.New(rA)
-	pubKeyA := privKeyA.Public()
+		// Create a private/pub key to sign
+		hFunc := goMimc //hash.MIMC_BN254.New("seed")
+		src1 := rand.NewSource(1)
+		src2 := rand.NewSource(2)
+		src3 := rand.NewSource(3)
+		rA := rand.New(src1)
+		rB := rand.New(src2)
+		rC := rand.New(src3)
 
-	privKeyB, err := signature.EDDSA_BN254.New(rB)
-	pubKeyB := privKeyB.Public()
+		privKeyA, err := signature.EDDSA_BN254.New(rA)
+		pubKeyA := privKeyA.Public()
 
-	privKeyC, err := signature.EDDSA_BN254.New(rC)
-	pubKeyC := privKeyC.Public()
+		privKeyB, err := signature.EDDSA_BN254.New(rB)
+		pubKeyB := privKeyB.Public()
 
-	/* Private and Public Key for A,B and C created */
+		privKeyC, err := signature.EDDSA_BN254.New(rC)
+		pubKeyC := privKeyC.Public()
 
-	// set values for all quotes
-	// quoteA, quoteB, QuoteC, winnerQuote - in this example, winner == quoteA
-	values := setQuotesValue(bond, "92.63", "93.4", "94")
+		/* Private and Public Key for A,B and C created */
 
-	//Set values for quotes from A,B and C
-	QuoteFromA := values[0]
-	QuoteFromB := values[1]
-	QuoteFromC := values[2]
+		//Set values for quotes from A,B and C
+		QuoteFromA := testCase.quoteA
+		QuoteFromB := testCase.quoteB
+		QuoteFromC := testCase.quoteC
 
-	signatureA, err := privKeyA.Sign(QuoteFromA[:], hFunc)
-	signatureB, err := privKeyB.Sign(QuoteFromB[:], hFunc)
-	signatureC, err := privKeyC.Sign(QuoteFromC[:], hFunc)
+		signatureA, err := privKeyA.Sign(QuoteFromA[:], hFunc)
+		signatureB, err := privKeyB.Sign(QuoteFromB[:], hFunc)
+		signatureC, err := privKeyC.Sign(QuoteFromC[:], hFunc)
 
-	id := ecc.BN254
+		id := ecc.BN254
 
-	// Seting up
-	var witness bondCircuitv5
+		// Seting up
+		var witness bondCircuitv5
 
-	witness.AcceptedQuote.Assign(values[0])
-	witness.Isin.Assign(IsinHash)
+		var IsinHash = testCase.isinTickerHash
+		witness.AcceptedQuote.Assign(testCase.winner)
+		witness.Isin.Assign(testCase.isinTickerHash)
 
-	goMimc.Reset()
-	goMimc.Write([]byte(IsinHash))
-	goMimc.Write([]byte(values[0]))
-	var IsinQuoteAHashed = goMimc.Sum(nil)
-	IsinQuoteSignedA, err := privKeyA.Sign(IsinQuoteAHashed[:], hFunc)
+		goMimc.Reset()
+		goMimc.Write([]byte(IsinHash))
+		goMimc.Write([]byte(testCase.quoteA))
+		var IsinQuoteAHashed = goMimc.Sum(nil)
+		IsinQuoteSignedA, err := privKeyA.Sign(IsinQuoteAHashed[:], hFunc)
 
-	goMimc.Reset()
-	goMimc.Write([]byte(IsinHash))
-	goMimc.Write([]byte(values[1]))
-	var IsinQuoteBHashed = goMimc.Sum(nil)
-	IsinQuoteSignedB, err := privKeyB.Sign(IsinQuoteBHashed[:], hFunc)
+		goMimc.Reset()
+		goMimc.Write([]byte(IsinHash))
+		goMimc.Write([]byte(testCase.quoteB))
+		var IsinQuoteBHashed = goMimc.Sum(nil)
+		IsinQuoteSignedB, err := privKeyB.Sign(IsinQuoteBHashed[:], hFunc)
 
-	goMimc.Reset()
-	goMimc.Write([]byte(IsinHash))
-	goMimc.Write([]byte(values[2]))
-	var IsinQuoteCHashed = goMimc.Sum(nil)
-	IsinQuoteSignedC, err := privKeyC.Sign(IsinQuoteCHashed[:], hFunc)
+		goMimc.Reset()
+		goMimc.Write([]byte(IsinHash))
+		goMimc.Write([]byte(testCase.quoteC))
+		var IsinQuoteCHashed = goMimc.Sum(nil)
+		IsinQuoteSignedC, err := privKeyC.Sign(IsinQuoteCHashed[:], hFunc)
 
-	sigRxt, sigRyt, sigS1t, sigS2t := parseSignature(id, IsinQuoteSignedA)
-	witness.IsinQuoteSignedA.R.X.Assign(sigRxt)
-	witness.IsinQuoteSignedA.R.Y.Assign(sigRyt)
-	witness.IsinQuoteSignedA.S1.Assign(sigS1t)
-	witness.IsinQuoteSignedA.S2.Assign(sigS2t)
+		sigRxt, sigRyt, sigS1t, sigS2t := parseSignature(id, IsinQuoteSignedA)
+		witness.IsinQuoteSignedA.R.X.Assign(sigRxt)
+		witness.IsinQuoteSignedA.R.Y.Assign(sigRyt)
+		witness.IsinQuoteSignedA.S1.Assign(sigS1t)
+		witness.IsinQuoteSignedA.S2.Assign(sigS2t)
 
-	sigRxt, sigRyt, sigS1t, sigS2t = parseSignature(id, IsinQuoteSignedB)
-	witness.IsinQuoteSignedB.R.X.Assign(sigRxt)
-	witness.IsinQuoteSignedB.R.Y.Assign(sigRyt)
-	witness.IsinQuoteSignedB.S1.Assign(sigS1t)
-	witness.IsinQuoteSignedB.S2.Assign(sigS2t)
+		sigRxt, sigRyt, sigS1t, sigS2t = parseSignature(id, IsinQuoteSignedB)
+		witness.IsinQuoteSignedB.R.X.Assign(sigRxt)
+		witness.IsinQuoteSignedB.R.Y.Assign(sigRyt)
+		witness.IsinQuoteSignedB.S1.Assign(sigS1t)
+		witness.IsinQuoteSignedB.S2.Assign(sigS2t)
 
-	sigRxt, sigRyt, sigS1t, sigS2t = parseSignature(id, IsinQuoteSignedC)
-	witness.IsinQuoteSignedC.R.X.Assign(sigRxt)
-	witness.IsinQuoteSignedC.R.Y.Assign(sigRyt)
-	witness.IsinQuoteSignedC.S1.Assign(sigS1t)
-	witness.IsinQuoteSignedC.S2.Assign(sigS2t)
+		sigRxt, sigRyt, sigS1t, sigS2t = parseSignature(id, IsinQuoteSignedC)
+		witness.IsinQuoteSignedC.R.X.Assign(sigRxt)
+		witness.IsinQuoteSignedC.R.Y.Assign(sigRyt)
+		witness.IsinQuoteSignedC.S1.Assign(sigS1t)
+		witness.IsinQuoteSignedC.S2.Assign(sigS2t)
 
-	AcceptedQuoteSigned, err := privKeyA.Sign(values[0][:], hFunc)
+		AcceptedQuoteSigned, err := privKeyA.Sign(testCase.winner[:], hFunc)
 
-	sigRx, sigRy, sigS1, sigS2 := parseSignature(id, AcceptedQuoteSigned)
-	witness.AcceptedQuoteSigned.R.X.Assign(sigRx)
-	witness.AcceptedQuoteSigned.R.Y.Assign(sigRy)
-	witness.AcceptedQuoteSigned.S1.Assign(sigS1)
-	witness.AcceptedQuoteSigned.S2.Assign(sigS2)
+		sigRx, sigRy, sigS1, sigS2 := parseSignature(id, AcceptedQuoteSigned)
+		witness.AcceptedQuoteSigned.R.X.Assign(sigRx)
+		witness.AcceptedQuoteSigned.R.Y.Assign(sigRy)
+		witness.AcceptedQuoteSigned.S1.Assign(sigS1)
+		witness.AcceptedQuoteSigned.S2.Assign(sigS2)
 
-	witness.WinnerQuote.Assign(values[0])
-	witness.Quote1.Assign(values[1])
-	witness.Quote2.Assign(values[2])
+		witness.WinnerQuote.Assign(testCase.winner)
+		witness.Quote1.Assign(testCase.quoteB)
+		witness.Quote2.Assign(testCase.quoteC)
 
-	witness.QuoteFromA.Assign(QuoteFromA)
-	witness.QuoteFromB.Assign(QuoteFromB)
-	witness.QuoteFromC.Assign(QuoteFromC)
+		witness.QuoteFromA.Assign(QuoteFromA)
+		witness.QuoteFromB.Assign(QuoteFromB)
+		witness.QuoteFromC.Assign(QuoteFromC)
 
-	//A
-	pubkeyAx, pubkeyAy := parsePoint(id, pubKeyA.Bytes())
-	var pbAx, pbAy big.Int
-	pbAx.SetBytes(pubkeyAx)
-	pbAy.SetBytes(pubkeyAy)
-	witness.PublicKeyA.A.X.Assign(pubkeyAx)
-	witness.PublicKeyA.A.Y.Assign(pubkeyAy)
+		//A
+		pubkeyAx, pubkeyAy := parsePoint(id, pubKeyA.Bytes())
+		var pbAx, pbAy big.Int
+		pbAx.SetBytes(pubkeyAx)
+		pbAy.SetBytes(pubkeyAy)
+		witness.PublicKeyA.A.X.Assign(pubkeyAx)
+		witness.PublicKeyA.A.Y.Assign(pubkeyAy)
 
-	witness.WinnerQuotePubKey.A.X.Assign(pubkeyAx)
-	witness.WinnerQuotePubKey.A.Y.Assign(pubkeyAy)
+		witness.WinnerQuotePubKey.A.X.Assign(pubkeyAx)
+		witness.WinnerQuotePubKey.A.Y.Assign(pubkeyAy)
 
-	sigRx, sigRy, sigS1, sigS2 = parseSignature(id, signatureA)
-	witness.SignatureA.R.X.Assign(sigRx)
-	witness.SignatureA.R.Y.Assign(sigRy)
-	witness.SignatureA.S1.Assign(sigS1)
-	witness.SignatureA.S2.Assign(sigS2)
+		sigRx, sigRy, sigS1, sigS2 = parseSignature(id, signatureA)
+		witness.SignatureA.R.X.Assign(sigRx)
+		witness.SignatureA.R.Y.Assign(sigRy)
+		witness.SignatureA.S1.Assign(sigS1)
+		witness.SignatureA.S2.Assign(sigS2)
 
-	//B
-	pubkeyBAx, pubkeyBAy := parsePoint(id, pubKeyB.Bytes())
-	var pbBAx, pbBAy big.Int
-	pbBAx.SetBytes(pubkeyBAx)
-	pbBAy.SetBytes(pubkeyBAy)
-	witness.PublicKeyB.A.X.Assign(pubkeyBAx)
-	witness.PublicKeyB.A.Y.Assign(pubkeyBAy)
+		//B
+		pubkeyBAx, pubkeyBAy := parsePoint(id, pubKeyB.Bytes())
+		var pbBAx, pbBAy big.Int
+		pbBAx.SetBytes(pubkeyBAx)
+		pbBAy.SetBytes(pubkeyBAy)
+		witness.PublicKeyB.A.X.Assign(pubkeyBAx)
+		witness.PublicKeyB.A.Y.Assign(pubkeyBAy)
 
-	sigBRx, sigBRy, sigBS1, sigBS2 := parseSignature(id, signatureB)
-	witness.SignatureB.R.X.Assign(sigBRx)
-	witness.SignatureB.R.Y.Assign(sigBRy)
-	witness.SignatureB.S1.Assign(sigBS1)
-	witness.SignatureB.S2.Assign(sigBS2)
+		sigBRx, sigBRy, sigBS1, sigBS2 := parseSignature(id, signatureB)
+		witness.SignatureB.R.X.Assign(sigBRx)
+		witness.SignatureB.R.Y.Assign(sigBRy)
+		witness.SignatureB.S1.Assign(sigBS1)
+		witness.SignatureB.S2.Assign(sigBS2)
 
-	//C
-	pubkeyCAx, pubkeyCAy := parsePoint(id, pubKeyC.Bytes())
-	var pbCAx, pbCAy big.Int
-	pbCAx.SetBytes(pubkeyCAx)
-	pbCAy.SetBytes(pubkeyCAy)
-	witness.PublicKeyC.A.X.Assign(pubkeyCAx)
-	witness.PublicKeyC.A.Y.Assign(pubkeyCAy)
+		//C
+		pubkeyCAx, pubkeyCAy := parsePoint(id, pubKeyC.Bytes())
+		var pbCAx, pbCAy big.Int
+		pbCAx.SetBytes(pubkeyCAx)
+		pbCAy.SetBytes(pubkeyCAy)
+		witness.PublicKeyC.A.X.Assign(pubkeyCAx)
+		witness.PublicKeyC.A.Y.Assign(pubkeyCAy)
 
-	sigCRx, sigCRy, sigCS1, sigCS2 := parseSignature(id, signatureC)
-	witness.SignatureC.R.X.Assign(sigCRx)
-	witness.SignatureC.R.Y.Assign(sigCRy)
-	witness.SignatureC.S1.Assign(sigCS1)
-	witness.SignatureC.S2.Assign(sigCS2)
+		sigCRx, sigCRy, sigCS1, sigCS2 := parseSignature(id, signatureC)
+		witness.SignatureC.R.X.Assign(sigCRx)
+		witness.SignatureC.R.Y.Assign(sigCRy)
+		witness.SignatureC.S1.Assign(sigCS1)
+		witness.SignatureC.S2.Assign(sigCS2)
 
-	// Generate Proof
-	proof, err := groth16.Prove(r1cs, pk, &witness)
+		// Generate Proof
+		proof, err := groth16.Prove(r1cs, pk, &witness)
 
-	//fmt.Println(proof)
-	if err != nil {
-		t.Fatal(err)
-	}
+		//fmt.Println(proof)
+		if err != nil {
+			fmt.Println("Test ", i, " failed.")
+		}
 
-	if vk == nil {
+		if err == nil {
 
-	}
-	//Check with a correct value and it returns NIL
-	var witnessCorrectValue bondCircuitv5
+			//Check with a correct value and it returns NIL
+			var witnessCorrectValue bondCircuitv5
 
-	witnessCorrectValue.AcceptedQuote.Assign(values[0])
+			witnessCorrectValue.AcceptedQuote.Assign(testCase.winner)
 
-	bond.Isin = "CA29250NAT24"
-	bond.Size = "550000"
-	bond.Ticker = "ENB 5.375 27-Sep-2077"
-	reqBodyBytes = new(bytes.Buffer)
-	json.NewEncoder(reqBodyBytes).Encode(bond)
+			IsinHash = testCase.isinTickerHash
+			witnessCorrectValue.Isin.Assign(IsinHash)
 
-	goMimc.Reset()
-	goMimc.Write([]byte(reqBodyBytes.Bytes()))
-	IsinHash = goMimc.Sum(nil)
-	witnessCorrectValue.Isin.Assign(IsinHash)
+			AcceptedQuoteSigned, err = privKeyA.Sign(testCase.winner[:], hFunc)
+			sigRx, sigRy, sigS1, sigS2 = parseSignature(id, AcceptedQuoteSigned)
+			witnessCorrectValue.AcceptedQuoteSigned.R.X.Assign(sigRx)
+			witnessCorrectValue.AcceptedQuoteSigned.R.Y.Assign(sigRy)
+			witnessCorrectValue.AcceptedQuoteSigned.S1.Assign(sigS1)
+			witnessCorrectValue.AcceptedQuoteSigned.S2.Assign(sigS2)
 
-	AcceptedQuoteSigned, err = privKeyA.Sign(values[0][:], hFunc)
-	sigRx, sigRy, sigS1, sigS2 = parseSignature(id, AcceptedQuoteSigned)
-	witnessCorrectValue.AcceptedQuoteSigned.R.X.Assign(sigRx)
-	witnessCorrectValue.AcceptedQuoteSigned.R.Y.Assign(sigRy)
-	witnessCorrectValue.AcceptedQuoteSigned.S1.Assign(sigS1)
-	witnessCorrectValue.AcceptedQuoteSigned.S2.Assign(sigS2)
+			witnessCorrectValue.PublicKeyA.A.X.Assign(pubkeyAx)
+			witnessCorrectValue.PublicKeyA.A.Y.Assign(pubkeyAy)
 
-	witnessCorrectValue.PublicKeyA.A.X.Assign(pubkeyAx)
-	witnessCorrectValue.PublicKeyA.A.Y.Assign(pubkeyAy)
+			witnessCorrectValue.PublicKeyB.A.X.Assign(pubkeyBAx)
+			witnessCorrectValue.PublicKeyB.A.Y.Assign(pubkeyBAy)
 
-	witnessCorrectValue.PublicKeyB.A.X.Assign(pubkeyBAx)
-	witnessCorrectValue.PublicKeyB.A.Y.Assign(pubkeyBAy)
+			witnessCorrectValue.PublicKeyC.A.X.Assign(pubkeyCAx)
+			witnessCorrectValue.PublicKeyC.A.Y.Assign(pubkeyCAy)
 
-	witnessCorrectValue.PublicKeyC.A.X.Assign(pubkeyCAx)
-	witnessCorrectValue.PublicKeyC.A.Y.Assign(pubkeyCAy)
-
-	err = groth16.Verify(proof, vk, &witnessCorrectValue)
-	if err != nil {
-		fmt.Print(err)
+			err = groth16.Verify(proof, vk, &witnessCorrectValue)
+			if err != nil {
+				fmt.Print(err)
+			}
+		}
 	}
 }
---
+++ testcases.go
@@ -0,0 +1,88 @@
+package financial
+
+import (
+	"bytes"
+	"encoding/json"
+
+	"github.com/consensys/gnark-crypto/hash"
+	"github.com/shopspring/decimal"
+)
+
+type TestCase struct {
+	quoteA         []byte
+	quoteB         []byte
+	quoteC         []byte
+	winner         []byte
+	isinTickerHash []byte
+}
+
+type Bond struct {
+	Isin   string
+	Size   string
+	Ticker string
+}
+
+func createTestCases() [3]TestCase {
+
+	toRet := [3]TestCase{}
+
+	bond := &Bond{
+		Isin:   "CA29250NAT24",
+		Size:   "550000",
+		Ticker: "ENB 5.375 27-Sep-2077",
+	}
+
+	// TODO - Quote A is always the winner, see how to change that
+	toRet[0] = getQuotesValue(bond, "92.63", "93.4", "90")
+	toRet[1] = getQuotesValue(bond, "92.63", "93.4", "94")
+	toRet[2] = getQuotesValue(bond, "92.63", "93.4", "94")
+
+	return toRet
+}
+
+func getQuotesValue(bond *Bond, quoteA string, quoteB string, quoteC string) TestCase {
+
+	quote0, err := decimal.NewFromString(quoteA)
+	quote1, err := decimal.NewFromString(quoteB)
+	quote2, err := decimal.NewFromString(quoteC)
+	if err != nil {
+		panic(err)
+	}
+
+	one100 := decimal.NewFromInt(100)
+	bondSize, err := decimal.NewFromString(bond.Size)
+
+	// 93.63 / 100 = 0,9363
+	quote0 = quote0.Div(one100)
+	quote1 = quote1.Div(one100)
+	quote2 = quote2.Div(one100)
+
+	//0.9363 * 550000 = 514965
+	quote0 = bondSize.Mul(quote0)
+	quote1 = bondSize.Mul(quote1)
+	quote2 = bondSize.Mul(quote2)
+
+	//convert to cents * 100
+	// 514965 * 100 = 51496500
+	quote0 = quote0.Mul(one100)
+	quote1 = quote1.Mul(one100)
+	quote2 = quote2.Mul(one100)
+
+	var testCase TestCase
+	testCase.quoteA = quote0.BigInt().Bytes()
+	testCase.quoteB = quote1.BigInt().Bytes()
+	testCase.quoteC = quote2.BigInt().Bytes()
+	testCase.winner = quote0.BigInt().Bytes()
+
+	reqBodyBytes := new(bytes.Buffer)
+	json.NewEncoder(reqBodyBytes).Encode(bond)
+
+	hashFunc := hash.MIMC_BN254
+
+	goMimc := hashFunc.New("seed")
+	goMimc.Write([]byte(reqBodyBytes.Bytes()))
+	var IsinHash = goMimc.Sum(nil)
+	testCase.isinTickerHash = IsinHash
+
+	return testCase
+}
